4f5e27e99d563dd4560d2770748bf68b0e07581e
Jonah Unuafe <fejisfejiro@gmail.com>
2024-02-02 14:29:31 +0100

    using cart context and reducer

---
+++ src/store/CartContext.jsx
@@ -0,0 +1,55 @@
+import { createContext, useReducer } from "react";
+
+const CartContext = createContext({
+    items: [],
+    addItem: (item) => {},
+    removeItem: (id) => {},
+});
+
+function cartReducer(state, action) {
+    if(action.type === "ADD_ITEM") {                            //this is totally up to me.
+        // ... update the state to add a meal item
+       const existingCartItemIndex = state.items.findIndex(
+            (item) => item.id === action.item.id
+        );
+
+        const updatedItems = [...state.items]
+
+        if(existingCartItemIndex > -1) {
+            const existingItem = state.items[existingCartItemIndex];
+            const updatedItem = {
+                ...existingItem,
+                quantity: existingItem.quantity + 1
+            };
+            updatedItems[existingCartItemIndex] = updatedItem;
+        } else {
+            updatedItems.push({ ...action.item, quantity: 1 });
+        }
+
+        return {
+            ...state,
+            items: updatedItems
+        };
+    }
+
+    if(action.type === "REMOVE_ITEM") {
+        // ... remove an item from the state
+    }
+
+    return state;
+}
+
+export function CartContextProvider({ children }) {
+    useReducer(cartReducer, { items: [] });
+
+
+    return (
+        <CartContext.Provider>{children}</CartContext.Provider>
+    );
+}
+
+export default CartContext;
+
+
+//CartContext is an object that have the Provider property. The Provider property is a component
+//that can be output and should be wrapped around any component that needs access to the CartContext
